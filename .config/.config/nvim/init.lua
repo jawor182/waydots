519f6410a232aab653c101bb27cee1f5eb8940d5
jawor182 <kubajaworski59@outlook.com>
2025-04-09 14:29:09 +0200

    clearinng

--- .config/.config/nvim/init.lua
+++
@@ -1,6 +0,0 @@
-require("config.lazy")
-require("config.keymap")
-require("config.opts")
----@diagnostic disable-next-line: different-requires
--- require("lazy").setup("plugins")
-
--- .config/.config/nvim/lua/plugins/dadbod.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "tpope/vim-dadbod",
-    "kristijanhusak/vim-dadbod-completion",
-    "kristijanhusak/vim-dadbod-ui",
-
-    config = function ()
-       vim.g.db_ui_use_nerd_fonts = 1
-    end
-}
